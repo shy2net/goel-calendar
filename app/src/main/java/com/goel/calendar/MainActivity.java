f0d7616a0352bf3dbc89cf12fa5f3c2d806be737
Shay Yaish <shynet@gmail.com>
2019-06-25 22:22:38 +0300

    Working username and password

--- app/src/main/java/com/goel/calendar/MainActivity.java
+++ app/src/main/java/com/goel/calendar/MainActivity.java
@@ -6,26 +6,30 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
-    EditText usernameField;
+    EditText emailField;
     EditText passwordField;
     Button signinButton;
+    FirebaseAuth firebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        firebaseAuth = FirebaseAuth.getInstance();
+
         /* Init all views */
-        usernameField = (EditText) findViewById(R.id.usernameField);
+        emailField = (EditText) findViewById(R.id.emailField);
         passwordField = (EditText) findViewById(R.id.passwordField);
         signinButton  = (Button) findViewById(R.id.signinButton);
         signinButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                firebaseAuth.signInWithEmailAndPassword(emailField.getText().toString(), passwordField.getText().toString());
             }
         });
 
